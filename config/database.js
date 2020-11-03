0f1550087dc1e6573ae140f0413202918a43669e
CarlosjVasquez <virahondacarlos@gmail.com>
2020-11-03 10:05:50 -0300

    update commit

--- config/database.js
+++ config/database.js
@@ -6,13 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "dacmqi118b066q"),
-        username: env("DATABASE_USERNAME", "fgijfjgygqdnvn"),
-        password: env(
-          "DATABASE_PASSWORD",
-          "ff7c18943008d18fae3c6045532b0d6961ef3d7f1dd2bd0b07fbadba6d203dd4"
-        ),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
       },
       options: {},
     },
