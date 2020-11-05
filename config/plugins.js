eed84ff37d07ad770e9e81a517fc9d1ef40a1c70
CarlosjVasquez <virahondacarlos@gmail.com>
2020-11-05 15:43:01 -0300

    add email send

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = () => ({
+  email: {
+    provider: "sendgrid",
+    providerOptions: {
+      apiKey:
+        "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",
+    },
+    settings: {
+      defaultFrom: "virahondacarlos@gmail.com",
+      defaultReplyTo: "virahondacarlos@gmail.com",
+    },
+  },
+});
