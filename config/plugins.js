9f6d9da19bdfcc280d7b927f6ccf6d18ae542cd5
CarlosjVasquez <virahondacarlos@gmail.com>
2020-12-18 18:56:26 -0500

    update email contact!

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,8 @@ module.exports = () => ({
         "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",
     },
     settings: {
-      defaultFrom: "contact@gmail.com",
-      defaultReplyTo: "contact@gmail.com",
+      defaultFrom: "contact@xibux.com",
+      defaultReplyTo: "contact@xibux.com",
     },
   },
 });
