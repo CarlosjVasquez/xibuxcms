00b35c871e1a70e372a9993e6c910debfc89e68c
Carlos Vasquez <virahondacarlos@gmail.com>
2021-03-29 12:41:17 -0500

    update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,3 +1,6 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
@@ -11,7 +14,9 @@ module.exports = ({ env }) => ({
         username: config.user,
         password: config.password,
       },
-      options: {},
+      options: {
+        ssl: false,
+      },
     },
   },
 });
